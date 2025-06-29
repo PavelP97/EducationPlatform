0120d182dae31ad606f1feceb6fb717a4f203bd8
pakhromov <pakhromov@gmail.com>
2025-06-29 20:27:05 +0200

    Added chemistry

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Card from './Card.jsx'
 import physics_icon from './assets/physics_icon.png'
 import math_icon from './assets/math_icon.png'
 import programming_icon from './assets/programming_icon.png'
+import chemistry_icon from './assets/chemistry_icon.png'
 
 function App() {
   return(
@@ -12,6 +13,7 @@ function App() {
       <Card name="Physics" icon={physics_icon}/>
       <Card name="Math" icon={math_icon}/>
       <Card name="Programming" icon={programming_icon}/>
+      <Card name="Chemistry" icon={chemistry_icon}/>
       <Footer/>
     </>
   )
